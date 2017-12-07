0950f19f3e906a62f0d3dfb3e1d1e688a42ff86b
maifuquan <maifuquan@ciwong.com>
2017-12-07 19:02:03 +0800

    test middleware

--- helloworld-mux/main.go
+++ helloworld-mux/main.go
@@ -4,18 +4,26 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	// "github.com/gorilla/mux"
+	"github.com/roobre/gorilla-mux"
 )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
+func MyHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Gorilla!\n"))
+}
+
+func simpleMW(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
-
+	r.HandleFunc("/", MyHandler)
+	// r.
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
