ffdba10bad060fdacd522d6b5f9ad3021bb3b19a
maifuquan <maifuquan@ciwong.com>
2018-03-19 12:16:42 +0800

    update

--- slice/main.go
+++ slice/main.go
@@ -6,7 +6,22 @@ import (
 )
 
 func main() {
+	tslice := []string{}
+	fmt.Printf("tslice []string{} len=%d,cap=%d\n", len(tslice), cap(tslice))
+	fmt.Printf("tslice pointer %p\n", tslice)
 	arr := [5]string{"a", "b", "c", "d", "e"}
+	tslice = arr[2:4]
+	fmt.Printf("tslice pointer %p\n", tslice)
+	tslice = append(tslice, "d")
+	fmt.Printf("tslice pointer %p\n", tslice)
+	tslice = append(tslice, "d")
+	fmt.Printf("tslice pointer %p\n", tslice)
+	fmt.Printf("tslice []string{} len=%d,cap=%d\n", len(tslice), cap(tslice))
+	tslice1 := append(tslice, "d")
+	fmt.Printf("tslice1 pointer %p\n", tslice1)
+	fmt.Printf("tslice1 []string{} len=%d,cap=%d\n", len(tslice1), cap(tslice1))
+	tslice2 := append(tslice, "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d")
+	fmt.Printf("tslice2 []string{} len=%d,cap=%d\n", len(tslice2), cap(tslice2))
 	aslice := arr[2:4]
 	bslice := arr[:4]
 	cslice := arr[4:]
