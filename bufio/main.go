e2a68c49e31996c4fb1f31835dcb8ddabc1428fc
maifuquan <maifuquan@ciwong.com>
2018-04-18 12:06:31 +0800

    add bufio, image

---
+++ bufio/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+		if len(counts) > 2 {
+			fmt.Println(counts)
+			break
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
