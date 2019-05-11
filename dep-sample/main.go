458b487e22e21bc8ec527f7de0fb224273592381
mark <kingmaidol@163.com>
2019-05-11 13:01:05 +0800

    add dep-sample

--- dep-sample/main.go
+++ dep-sample/main.go
@@ -14,6 +14,9 @@ func FieldMap(req *http.Request) binding.FieldMap {
 
 func main() {
 	r := mux.NewRouter()
-	r.Handle("/", http.FileServer(http.Dir(".")))
+	// r.Handle("/", http.FileServer(http.Dir(".")))
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world!"))
+	})
 	http.ListenAndServe(":"+os.Getenv("PORT"), r)
 }
